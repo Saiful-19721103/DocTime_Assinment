eb39da9850e008bbf5145fd73bfa83bc549e331e
Saiful <saiful197583@gmail.com>
2022-08-01 23:41:51 +0600

    Home Page Primary Setp

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    /**
+     * Show Home Page
+     */
+    public function showHomePage()
+    {
+        return view('frontend.home');
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
+//Frontend Controller
+Route::get('/', [ FrontendController::class, 'showHomePage'])->name('home.page');
